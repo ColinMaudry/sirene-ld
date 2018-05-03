34f635f94ab5710cc5b4372edf86ff32a953898f
Colin Maudry <colin@maudry.com>
2018-05-03 19:33:14 +0200

    Script de conversion CSV > TTL > HDT pour département ou tout #1 #22

--- scripts/téléchargerUnzip.sh
+++ scripts/téléchargerUnzip.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
 departement=$1
-filename=geo-sirene_$departement
-csv=$filename.csv
-zip=$csv.7z
+
+if [ $departement = "tout"  ]
+then
+	filename=geo_sirene
+	csv=$filename.csv
+	zip=$csv.gz
+	unzip="gzip -d"
+else
+	filename=geo-sirene_$1
+        csv=$filename.csv
+        zip=$csv.7z
+	unzip="p7zip -d"
+
+echo "Code département : $departement"
+echo ""
+
+fi
+
 ttl=$filename.ttl
 hdt=$filename.hdt
 
 
 rm $filename*
 
-echo "Code département : $departement"
-echo ""
 echo "> Téléchargement du fichier compressé..."
 
 wget http://data.cquest.org/geo_sirene/last/$zip
@@ -19,7 +32,7 @@ wget http://data.cquest.org/geo_sirene/last/$zip
 echo ""
 echo "> Extraction du fichier compressé..."
 
-p7zip -d $zip
+$unzip $zip
 
 echo ""
 echo "> Conversion du CSV en RDF (Turtle)..."
