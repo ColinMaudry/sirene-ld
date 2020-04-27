eb633b33cca864241c8d0d687f5181bda6d10ff0
Colin Maudry <colin@maudry.com>
2020-04-27 22:22:51 +0200

    Improved logging in hdt.sh #44

--- scripts/hdt.sh
+++ scripts/hdt.sh
@@ -2,6 +2,11 @@
 root=`pwd`
 gz=${rdf}.gz
 gzFile=${rdfFile}.gz
+hdt=$root/hdt/sireneld.hdt
+
+
+export step=hdt
+source $root/scripts/functions.sh
 
 
 if [[ -z $test ]]
@@ -18,8 +23,8 @@ else
 fi
 
 
-echo "server=$server"
-echo "branch=$branch"
+notify "server=$server"
+notify "branch=$branch"
 
 if [[ ! -d hdt ]]
 then
@@ -28,8 +33,8 @@ fi
 
 function makeHdt {
     gz=$1
-    echo "$server: about to process $gz for HDT conversion."
-    time rdf2hdt -i -f nt "$gz" $root/hdt/sireneld.hdt
+    notify "$server: about to process $gz for HDT conversion."
+    time rdf2hdt -i -f nt "$gz" $hdt
 }
 
 case $server in
@@ -39,35 +44,42 @@ case $server in
         then
             mkdir rdf
         fi
-        echo "$server: pwd=$(pwd)"
-        echo "$server: gz=$gz"
+        notify "$server: pwd=$(pwd)"
+        notify "$server: gz=$gz"
         ls rdf
         makeHdt $gz
-
-        echo "done" > finished
+        
+        if [[ -f $hdt ]]
+        then
+          notify "$server: HDT complete"
+        else
+          notify "$server: HDT creation failed"
+        fi
+        notify "done" > finished
     ;;
 
     main)
         datetime=`date "+%FT%T"`
         name="hdt-cpp-server-$datetime"
 
-        echo "$server: Creating dedicated instance in the background..."
+        notify "$server: Creating dedicated instance in the background (Scaleway $scalewayType)..."
         id=`scw start $(scw create --name "$name" $scalewayType $image)`
         scw exec -w $id "cd sirene-ld && git pull && git checkout $branch && git pull origin $branch && mkdir rdf"
 
-        echo "$server: Server created and started."
+        notify "$server: Server created and started."
 
         # Clear cache (see bug in scw: https://github.com/scaleway/scaleway-cli/issues/531)
         rm ~/.scw-cache.db
 
         ip=`scw inspect "$id" | jq -r '.[0].public_ip.address'`
 
-        echo "$server: Sending $gz to the server..."
+        notify "$server: Sending $gz to the server..."
         scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q $gz root@${ip}:/root/sirene-ld/rdf/
+        notify "$server: $gz sent"
 
-        echo "$server: $(date +%H:%M:%S): starting HDT creation... ${type}..."
+        notify "$server: starting HDT creation... "
         scw exec -w $id "cd /root/sirene-ld && make hdtOnly branch="$branch" server='hdt'" &
-        echo "$server: HDT processing started..."
+        notify "$server: HDT processing started..."
         #wait for HDT
 
 
@@ -76,24 +88,24 @@ case $server in
             # I wish I could use scw cp but... : https://github.com/scaleway/scaleway-cli/issues/537
             scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q root@${ip}:/root/sirene-ld/finished . 2>&1 | grep "xx"
 
-            sleep 10
+            sleep 30
         done
 
-        echo "$server: $(date +%H:%M:%S): finished HDT creation."
+        notify "$server: finished HDT creation."
         rm finished
-        echo "Downloading HDT from instance server..."
+        notify "Downloading HDT from instance server..."
         # Download HDT from Scaleway server
         scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q root@${ip}:/root/sirene-ld/hdt/* ./hdt/
-        echo "done"
+        notify "HDT downloaded"
 
-        echo "Deleting instance server..."
+        notify "Deleting instance server (Scaleway $scalewayType)..."
         # Delete the server
         scw rm -f "$id"
-        echo "done"
+        notify "Instance server deleted"
     ;;
 
     *)
-        echo "Processing HDT locally..."
+        notify "Processing HDT locally..."
         makeHdt
 
     ;;
