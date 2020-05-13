545b552a633fb6e1cba04866392a55a4769d537d
Colin Maudry <colin@maudry.com>
2020-05-13 19:13:45 +0200

    gzipped output #40

--- scripts/sources/decp/rdf.sh
+++ scripts/sources/decp/rdf.sh
@@ -7,7 +7,7 @@ set -e
 
 notify "Output: $sourceRdf"
 
-jq -r -f $root/scripts/sources/$source/rdf.jq  $source.json > $sourceRdf
+jq -r -f $root/scripts/sources/$source/rdf.jq  $source.json | gzip -9 > $sourceRdf
 
 notify "Finished step"
 
