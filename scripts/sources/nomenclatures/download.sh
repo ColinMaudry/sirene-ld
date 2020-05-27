c17d25c9d53065ae6e63e8723bbf37aa7a27db89
Colin Maudry <colin@maudry.com>
2020-05-27 20:16:10 +0200

    Fixed sed command to replace all occurences in a line #41

--- scripts/sources/nomenclatures/download.sh
+++ scripts/sources/nomenclatures/download.sh
@@ -10,7 +10,7 @@ curl -sL http://cpv.data.ac.uk/turtle/dump.ttl -o cpv.ttl
 
 notify "Adapting UK CPV data to sireneld (URIs)..."
 sed -i "s/http\:\/\/purl.org\/cpv\/2008/https:\/\/sireneld.io\/cpv/" cpv.ttl
-sed -i "s/code\-//" cpv.ttl
+sed -i "s/code\-//g" cpv.ttl
 
 notify "Copying /nomenclatures/* to /sources/nomenclatures..."
 cp $root/nomenclatures/* .
