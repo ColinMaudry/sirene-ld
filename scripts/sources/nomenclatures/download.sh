8049f4c49a0f51f842e7b0efa9b373f80eb4327c
Colin Maudry <colin@maudry.com>
2020-05-27 20:12:33 +0200

    Modify CPV URIs after download #41

--- scripts/sources/nomenclatures/download.sh
+++ scripts/sources/nomenclatures/download.sh
@@ -8,6 +8,10 @@ set -e
 notify "CPV: downloading CPV data from http://cpv.data.ac.uk/..."
 curl -sL http://cpv.data.ac.uk/turtle/dump.ttl -o cpv.ttl
 
+notify "Adapting UK CPV data to sireneld (URIs)..."
+sed -i "s/http\:\/\/purl.org\/cpv\/2008/https:\/\/sireneld.io\/cpv/" cpv.ttl
+sed -i "s/code\-//" cpv.ttl
+
 notify "Copying /nomenclatures/* to /sources/nomenclatures..."
 cp $root/nomenclatures/* .
 
